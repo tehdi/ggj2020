bbb69bb3283ea354e64b15c4a8671dcd5b9c4c14
tehdi <tehdi@users.noreply.github.com>
2020-02-01 15:33:09 -0400

    randomize mission selection, re-choose all 3 when one is completed

--- src/App.js
+++ src/App.js
@@ -1,57 +1,104 @@
 import React from 'react';
 import './App.css';
-import game_data from './gameData.js';
+import gameData from './gameData.js';
 
 class App extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            temperature: game_data.starting_temperature,
-            rate: game_data.starting_rate,
-            event_log: []
+            score: gameData.startingScore,
+            rate: gameData.startingRate,
+            availableMissions: this.selectMissions(),
         }
     }
 
-    endTurn = () => {
-        const start_temp = this.state.temperature;
-        const rate = this.state.rate;
-        this.setState({ temperature: start_temp + rate });
-    }
-
-    drawCard = () => {
-        const events = game_data.events;
-        const event = game_data.events[Math.floor(Math.random() * events.length)];
-        this.state.event_log.unshift(event);
-    }
-
-    endClimateChange = () => {
-        this.setState({
-            temperature: 10,
-            rate: 0
-        });
-    }
-
     render() {
-        // const event_log = this.state.event_log
-        //     .map(event => {
-        //         console.log(event);
-        //         return <span key={event.id}>{event.description}</span>
-        //     });
+        const availableMissions = this.selectMissions();
+
+        const missionComponents = availableMissions
+            .map(mission => {
+                return <Mission
+                    key={mission.id}
+                    missionId={mission.id}
+                    missionDescription={mission.description}
+                    onCompleteMission={this.completeMission} />
+            });
 
         return (
             <div className="App">
                 <div id="stateDisplay">
-                    <div>{this.state.temperature} ({this.state.rate < 0 ? "-" : ""}{this.state.rate} / turn)</div>
-                    {/* <div>{this.state.event_log}</div> */}
+                    <div>{this.state.score} ({this.state.rate < 0 ? "" : "+"}{this.state.rate} / turn)</div>
                 </div>
-                <div id="turnActions">
-                    <button onClick={this.drawCard}>Draw an Event Card</button>
+
+                <div>
+                    {missionComponents}
                 </div>
-                <button onClick={this.endTurn}>End Turn</button>
+
                 <button onClick={this.endClimateChange}>End Climate Change</button>
             </div>
         );
+    }
+
+    selectMissions = () => {
+        // for now we're not worried about re-presenting the same mission multiple times
+        // but at any given point, we should show three different ones
+        const allMissions = gameData.missions;
+        const missionIds = new Set();
+        while (missionIds.size < 3) {
+            missionIds.add(Math.floor(Math.random() * allMissions.length));
+        }
+
+        const selectedMissions = [];
+        missionIds.forEach(missionId => {
+            selectedMissions.push(allMissions[missionId]);
+        })
+        return selectedMissions;
+    }
+
+    completeMission = (missionId) => {
+        console.log("Completing " + missionId);
+        const completedMission = gameData.missions
+            .find(mission => missionId === mission.id);
+
+        this.setState({
+            score: this.getNewScore(completedMission),
+            rate: this.getNewRate(completedMission),
+            availableMissions: this.selectMissions()
+        });
+    }
+
+    getNewScore = (mission) => {
+        const scoreChange = mission.scoreImpact;
+        return this.state.score + scoreChange;
+    }
+
+    getNewRate = (mission) => {
+        const rateChange = mission.rateImpact;
+        return this.state.rate + rateChange;
+    }
+
+    endClimateChange = () => {
+        this.setState({
+            score: 10,
+            rate: 0,
+            availableMissions: []
+        });
+    }
+}
+
+class Mission extends React.Component {
+    completeMission = () => {
+        this.props.onCompleteMission(this.props.missionId);
+    }
+
+    render() {
+        return (
+            <div className="missionBox">
+                <div>Mission {this.props.missionId}: {this.props.missionDescription}</div>
+                <button onClick={this.completeMission}>Complete!</button>
+            </div>
+        )
     }
 }
 
--- src/gameData.js
+++ src/gameData.js
@@ -1,39 +1,99 @@
-const game_data = {
-    starting_temperature: 20,
-    starting_rate: 1,
-    events: [
+const gameData = {
+    startingScore: 20,
+    goalScore: 10,
+    failScore: 100,
+    startingRate: 2,
+    missions: [
         {
             id: 0,
-            description: "One of your enviro-repair mechs has broken down",
-            temperature_impact: 1,
-            rate_impact: 0
+            description: "Sign the Kyoto Protocol",
+            scoreImpact: 0,
+            rateImpact: -1,
+            // description, background image (maybe), and requirements will be on the card image
         },
         {
             id: 1,
-            description: "Wildfires have broken out in Australia",
-            temperature_impact: 2,
-            rate_impact: 1
+            description: "Extinguish the Australian wildfires",
+            scoreImpact: -1,
+            rateImpact: 0,
         },
         {
             id: 2,
-            description: "",
-            temperature_impact: -1,
-            rate_impact: 0
+            description: "Sign the Paris Accord",
+            scoreImpact: 0,
+            rateImpact: -1,
         },
-        // bribe a politician
-        // topple a fascist regime
-        // elect a new US president
-        // sign the Paris Accord
-        // sign the Kyoto Accord
+        {
+            id: 3,
+            description: "Start a coup",
+            scoreImpact: 1,
+            rateImpact: -1,
+        },
+        {
+            id: 4,
+            description: "Build a solar energy farm",
+            scoreImpact: -1,
+            rateImpact: -1,
+        },
+        {
+            id: 5,
+            description: "Build a wind energy farm",
+            scoreImpact: -1,
+            rateImpact: -1,
+        },
+        {
+            id: 6,
+            description: "Stop malaria outbreak",
+            scoreImpact: 0,
+            rateImpact: 0,
+        },
+        {
+            id: 7,
+            description: "Lease a rainforest",
+            scoreImpact: 0,
+            rateImpact: -1,
+        },
+        {
+            id: 8,
+            description: "Plant trees",
+            scoreImpact: -1,
+            rateImpact: -1,
+        },
+        {
+            id: 9,
+            description: "Sabotage a coal power plant",
+            scoreImpact: 0,
+            rateImpact: -1,
+        },
+        {
+            id: 10,
+            description: "Open a recycling plant",
+            scoreImpact: 0,
+            rateImpact: -1,
+        },
+        /*
+        {
+            id: ,
+            description: "",
+            scoreImpact: ,
+            rateImpact: ,
+        },
+        */
     ],
-    objectives: [
+    random_events: [
         {
             id: 0,
-            description: "",
-            requirement: "",
-            reward: ""
-        }
+            description: "Thanos snap",
+            scoreImpact: 5,
+            rateImpact: -5,
+        },
+        {
+            id: 1,
+            description: "Mount Vesuvius erupts",
+            scoreImpact: 0,
+            rateImpact: 0,
+        },
     ]
 };
 
-export default game_data;
+export default gameData;
